89b0cb87ecc63dae418abef40e20bd39fe587865
hamzazzzz2022-pixel <hamzazzzz2022@gmail.com>
2026-09-04 02:11:36 +0300

    Create sitemap.js

---
+++ src/app/sitemap.js
@@ -0,0 +1,24 @@
+export default function sitemap() {
+  const baseUrl = 'https://zakirli-online.vercel.app';
+
+  return [
+    {
+      url: baseUrl,
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1,
+    },
+    {
+      url: `${baseUrl}/login`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/signup`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+  ];
+}
