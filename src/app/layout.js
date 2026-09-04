9b2cbd0dbd4cbff9e63957bbf8d6b71a8d99987e
hamzazzzz2022-pixel <hamzazzzz2022@gmail.com>
2026-09-05 02:30:14 +0300

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,11 @@
 import "./globals.css";
 
 export const metadata = {
-  title: "منصة الخلاصة 📃",
+  title: "منصة الخلاصة 🎓",
   description: "منصتك التعليمية الذكية",
+  icons: {
+    icon: 'data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="%233b82f6"><path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"/></svg>',
+  },
 };
 
 export default function RootLayout({ children }) {
